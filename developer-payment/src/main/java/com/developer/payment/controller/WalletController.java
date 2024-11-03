ac8a50c926755722ea45b2f57043c7df78e6c206
liao <17608478306@163.com>
2024-11-03 14:06:29 +0800

    开通钱包

--- developer-payment/src/main/java/com/developer/payment/controller/WalletController.java
+++ developer-payment/src/main/java/com/developer/payment/controller/WalletController.java
@@ -1,6 +1,8 @@
 package com.developer.payment.controller;
 
 import com.developer.framework.model.DeveloperResult;
+import com.developer.payment.service.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("wallet")
 public class WalletController {
 
+    @Autowired
+    private WalletService walletService;
 
     /**
      * 开通钱包
@@ -16,7 +20,7 @@ public class WalletController {
      */
     @PostMapping("create")
     public DeveloperResult<Boolean> CreateWallet(){
-        return DeveloperResult.success();
+        return walletService.CreateWallet();
     }
 
 
