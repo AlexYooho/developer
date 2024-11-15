78c28ec4b6b20013e655a9789cd1def0a3dd0bb0
liao <17608478306@163.com>
2024-11-15 11:02:14 +0800

    MinIOBean重命名MinioClientConfig

--- developer-oss/src/main/java/com/developer/oss/config/MinIOClientConfig.java
+++ developer-oss/src/main/java/com/developer/oss/config/MinIOClientConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.Resource;
 
 @Configuration
-public class MinIOBean {
+public class MinIOClientConfig {
 
     @Resource
     private MinioConfig minioProp;
