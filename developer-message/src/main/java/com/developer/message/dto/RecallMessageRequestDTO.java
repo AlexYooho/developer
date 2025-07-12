d5d3df590987578be21c8ec041422ec818e657e3
liao <17608478306@163.com>
2025-07-12 16:58:28 +0800

    优化哦

--- developer-message/src/main/java/com/developer/message/dto/RecallMessageRequestDTO.java
+++ developer-message/src/main/java/com/developer/message/dto/RecallMessageRequestDTO.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
 public class RecallMessageRequestDTO {
-    @JsonProperty("serial_no")
-    private String serialNo;
-
     @JsonProperty("message_id")
     private Long messageId;
 }
