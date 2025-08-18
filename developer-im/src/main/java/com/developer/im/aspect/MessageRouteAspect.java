b2b45bcfa9719bef3a4561becb683109686bf60f
liao <17608478306@163.com>
2025-08-18 15:25:22 +0800

    perf:一次大的调整 消息路由优化

--- developer-im/src/main/java/com/developer/im/aspect/MessageRouteAspect.java
+++ developer-im/src/main/java/com/developer/im/aspect/MessageRouteAspect.java
@@ -34,7 +34,7 @@ public class MessageRouteAspect {
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
 
         Object[] args = joinPoint.getArgs();
-        RabbitMQMessageBodyDTO dto =(RabbitMQMessageBodyDTO)args[0];
+        RabbitMQMessageBodyDTO dto = (RabbitMQMessageBodyDTO) args[0];
         ChatMessageDTO chatMessageDTO = dto.parseData(ChatMessageDTO.class);
 
         String localIPv4 = IPUtils.getLocalIPv4();
@@ -42,7 +42,7 @@ public class MessageRouteAspect {
         String localUrl = localIPv4.concat(localPort);
 
         /** 格式 Map<URL,[user1,user2,user3]></>*/
-        Map<String,List<Long>> transpondMap = new HashMap<>();
+        Map<String, List<Long>> transpondMap = new HashMap<>();
         List<Long> removeList = new ArrayList<>();
 
 
@@ -53,7 +53,7 @@ public class MessageRouteAspect {
             for (Integer terminal : terminals) {
                 String key = RedisKeyConstant.USER_MAP_SERVER_INFO_KEY(receiverId);
                 Object serverUrl = redisUtil.hGet(key, terminal.toString());
-                if(ObjectUtil.isEmpty(serverUrl)){
+                if (ObjectUtil.isEmpty(serverUrl)) {
                     continue;
                 }
 
@@ -61,48 +61,48 @@ public class MessageRouteAspect {
                 String targetUrl = Optional.ofNullable(serverUrl).orElse("").toString();
 
                 // 当前节点不处理
-                if(localUrl.equals(targetUrl)){
+                if (localUrl.equals(targetUrl)) {
                     continue;
                 }
 
                 existTerminalCnt++;
 
-                if(transpondMap.containsKey(targetUrl)){
-                    List<Long> ids = transpondMap.get(targetUrl);
-                    if(ids.contains(receiverId)){
-                       continue;
-                    }
-
-                    ids.add(receiverId);
+                if (!transpondMap.containsKey(targetUrl)) {
+                    transpondMap.put(targetUrl, Collections.singletonList(receiverId));
                     continue;
                 }
 
-                transpondMap.put(targetUrl, Collections.singletonList(receiverId));
+                List<Long> ids = transpondMap.get(targetUrl);
+                if (ids.contains(receiverId)) {
+                    continue;
+                }
+                ids.add(receiverId);
             }
 
             // 记录用户任何terminal都不在此server上的receiverId
-            if(existTerminalCnt>=0){
+            if (existTerminalCnt >= 0) {
                 removeList.add(receiverId);
             }
         }
 
         chatMessageDTO.getReceiverIds().removeAll(removeList);
 
-        if(CollUtil.isEmpty(chatMessageDTO.getReceiverIds())){
+        if (CollUtil.isEmpty(chatMessageDTO.getReceiverIds())) {
             return null;
         }
 
+        if (MapUtil.isEmpty(transpondMap)) {
+            return joinPoint.proceed();
+        }
 
         // 调用目标服务端
-        if(MapUtil.isNotEmpty(transpondMap)){
-            for (Map.Entry<String,List<Long>> entry : transpondMap.entrySet()){
-                String targetUrl = entry.getKey();
-                // 远程调用目标server,可以通过Dubbo、feign都可以
-                IMRpcService instance = RpcUtil.getInstance(IMRpcService.class, targetUrl);
-                chatMessageDTO.setReceiverIds(entry.getValue());
-                dto.setData(chatMessageDTO);
-                instance.pushTargetWSNode(dto);
-            }
+        for (Map.Entry<String, List<Long>> entry : transpondMap.entrySet()) {
+            String targetUrl = entry.getKey();
+            // 远程调用目标server,可以通过Dubbo、feign都可以
+            IMRpcService instance = RpcUtil.getInstance(IMRpcService.class, targetUrl);
+            chatMessageDTO.setReceiverIds(entry.getValue());
+            dto.setData(chatMessageDTO);
+            instance.pushTargetWSNode(dto);
         }
 
         return joinPoint.proceed();
